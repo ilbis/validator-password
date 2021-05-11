4c3e75eab74efe774e4b6681cec21bd3ea02cefe
ilbis <devops@santander.com.br>
2021-05-10 21:15:33 -0300

    Initial commit

---
+++ src/main/java/com/itau/validatorpassword/ValidatorPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.itau.validatorpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorPasswordApplication.class, args);
+	}
+
+}
